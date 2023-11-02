7b1b68475c833048d160e59cb23c134a3cfc65c3
Daniel Jara <daniel11jara@gmail.com>
2023-11-02 12:10:42 -0300

    manutencao

--- curso_jsp4/src/main/java/servlets/ServletUsuarioController.java
+++ curso_jsp4/src/main/java/servlets/ServletUsuarioController.java
@@ -1,18 +1,19 @@
 package servlets;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dao.DAOUsuarioRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
-import java.io.IOException;
-import java.util.List;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dao.DAOUsuarioRepository;
 
-@WebServlet(urlPatterns = {"/ServletUsuarioController"})
+//@WebServlet(urlPatterns = {"/ServletUsuarioController"})
 public class ServletUsuarioController extends ServletGenericUtil {
 	private static final long serialVersionUID = 1L;
 	
